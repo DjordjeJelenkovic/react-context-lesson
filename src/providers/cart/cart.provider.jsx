161d52fe4468f3be9a7ad6c8d91f771d4e98dad5
Djordje Jelenkovic <ledjo.jelenkovic@gmail.com>
2021-10-06 12:48:57 +0200

    7. Provider Context Pattern 2

--- src/providers/cart/cart.provider.jsx
+++ src/providers/cart/cart.provider.jsx
@@ -3,6 +3,8 @@ import React, { createContext, useState, useEffect } from 'react';
 import {
   addItemToCart,
   removeItemFromCart,
+  filterItemFromCart,
+  getCartItemsCount
 } from './cart.utils';
 
 export const CartContext = createContext({
@@ -21,7 +23,14 @@ const CartProvider = ({ children }) => {
   const [cartItemsCount, setCartItemsCount] = useState(0);
 
   const addItem = item => setCartItems(addItemToCart(cartItems, item));
+  const removeItem = item => setCartItems(removeItemFromCart(cartItems, item));
   const toggleHidden = () => setHidden(!hidden);
+  const clearItemFromCart = item =>
+    setCartItems(filterItemFromCart(cartItems, item));
+
+    useEffect(() => {
+      setCartItemsCount(getCartItemsCount(cartItems));
+    }, [cartItems]);
 
   return (
     <CartContext.Provider
@@ -30,7 +39,9 @@ const CartProvider = ({ children }) => {
         toggleHidden,
         cartItems,
         addItem,
-        cartItemsCount
+        removeItem,
+        cartItemsCount,
+        clearItemFromCart
       }}
     >
       {children}
